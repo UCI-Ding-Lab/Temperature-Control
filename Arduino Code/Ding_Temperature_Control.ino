b785637d70ec32d25c6885db420083056f7dbb7a
Tony Liu <18509861+T9065@users.noreply.github.com>
2023-11-13 13:26:55 -0800

    Update Ding_Temperature_Control.ino

--- Arduino Code/Ding_Temperature_Control.ino
+++ Arduino Code/Ding_Temperature_Control.ino
@@ -1,7 +1,9 @@
 /********************************************************
-   PID Temperature Control
+   PID Temperature Control for Microscopes, Microfluidic Chambers, and Custom Incubators
    By: Tony Liu @ Ding Lab
  ********************************************************/
+
+// Include libraries for sensors, LCD, and PID
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Wire.h>
@@ -9,17 +11,19 @@
 #include <LiquidCrystal_I2C.h>
 #include <QuickPID.h>
 
-//Pins
+// Declare pin numbers
 const int PROBEPIN = 9;
 const int HEATERPIN = 6;
 const int TARGETPIN = 1;
 
-//Variables
+// Declare tempearture variables
 float targetTemp, Input, Output;
 double probeTemp, mcpTemp, mcpTemp2, avgTemp;
-float Kp = 505, Ki = 62, Kd = 92; //Change this according to your setup
 
-//Constructors for sensors
+// PID gains; change these according to your setup
+float Kp = 505, Ki = 62, Kd = 92;
+
+// Constructors for sensors
 OneWire oneWire(PROBEPIN);
 DallasTemperature probe(&oneWire);
 LiquidCrystal_I2C lcd(0x27,20,4);
@@ -27,58 +31,64 @@ Adafruit_MCP9808 mcp = Adafruit_MCP9808();
 Adafruit_MCP9808 mcp2 = Adafruit_MCP9808();
 QuickPID myPID(&Input, &Output, &targetTemp);
 
-//Helper function to calculate double for map function, use this if you need decimal target temperature
-double mapf(double x, double in_min, double in_max, double out_min, double out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-//Takes in potentiometer input and converts to 20-60 Degree target temperature
+// Reads potentiometer input and converts to 20-60 Degree for the target temperature
 void changeTargetTemp()
 {
-  int targetInput = analogRead(TARGETPIN);//1023 - smoothInput();
-  // Serial.print(targetInput);
+  // Read potentiometer input from 0 to 1023
+  int targetInput = analogRead(TARGETPIN);
+
+  // Converts potentiometer input to 20-60 degree target temperature
   targetTemp = map(targetInput, 0, 1023, 20, 60);
 }
 
 //Reads all temperature sensors and potentiometer
 void readInput() 
 {
-  //potentiometer reading and display
+  // Reads the target temperature from the potentiometer
   changeTargetTemp();
+
+  // Prints the target temperature to the serial monitor
   Serial.print("Target:");
   Serial.print(targetTemp);
   Serial.print(" ");
 
+  // Prints the target temperature to the top right of the LCD
   lcd.setCursor(9, 0);
   lcd.print("T:");
   lcd.print(targetTemp, 2);
-  // lcd.print('C');
 
-  //Internal Probe
+  // Updates the temperature reading from the probe sensor
   probe.requestTemperatures(); 
   probeTemp = probe.getTempCByIndex(0);
+
+  // Prints the probe temperature to the serial monitor
   Serial.print("Internal: ");
   Serial.print(probeTemp, 3);
   Serial.print(" ");
   
-  //Surface MCP Setup
+  // Updates the temperature reading from the surface MCP sensor
   mcpTemp = mcp.readTempC();
+
+  // Prints the surface MCP temperature to the serial monitor
   Serial.print("Surface:");
   Serial.print(mcpTemp, 3);
   Serial.print(" ");
   
-  //Ambient MCP Setup
+  // Updates the temperature reading from the ambient MCP sensor
   mcpTemp2 = mcp2.readTempC();
+
+  // Prints the ambient MCP temperature to the serial monitor
   Serial.print("Ambient:");
   Serial.print(mcpTemp2, 3);
   Serial.print(" ");
   
+  // Prints the ambient MCP temperature to the bottom left of the LCD
   lcd.setCursor(0, 1);
   lcd.print("A:");
   lcd.print(mcpTemp2, 2);
 
-  if(probeTemp > 15 && mcpTemp > 15)
+  // Calculates the average temperature between the probe and surface MCP
+  if(probeTemp > 15 && mcpTemp > 15) // Only calculates average if both sensors are above 15 degrees; prevents unstable readings
   {
     avgTemp = (probeTemp + mcpTemp) / 2.0;  
     Input = avgTemp;
@@ -86,19 +96,24 @@ void readInput()
     Serial.print(avgTemp, 3);
     Serial.println("  ");
     
+    // Prints the average temperature to the top left of the LCD
     lcd.setCursor(0, 0);
     lcd.print("I:");
     lcd.print(avgTemp, 2);
   }
 }
 
+// Setup function runs once at the beginning of the program
+// Initializes sensors, LCD, and PID
 void setup()
 {  
+  // Initialize serial monitor, LCD, and probe sensor
   Serial.begin(9600);
   while(!Serial);
   lcd.begin(16, 2);
   probe.begin();
 
+  // Initialize MCP sensors; if not found, print error message and pause program
   if (!mcp.begin(0x18)) 
   {
     Serial.println("Couldn't find MCP9808!");
@@ -110,33 +125,43 @@ void setup()
     while (1);
   }
   
+  // Set MCP sensor resolution to 0.0625 °C
   mcp.setResolution(3);
   mcp2.setResolution(3);
   
+  // Set heater and target pins to output mode
   pinMode(HEATERPIN, OUTPUT);
   pinMode(TARGETPIN, INPUT);
   
+  // Initialize LCD and turn on backlight
   lcd.init();
   lcd.backlight();
 
-    targetTemp = 25;
+  // Set initial target temperature to 25 degrees
+  targetTemp = 25;
 
-  //apply PID gains
+  // Apply PID gains
   myPID.SetTunings(Kp, Ki, Kd);
 
-  //turn the PID on
+  // Turn the PID on
   myPID.SetMode(myPID.Control::automatic);
 }
 
+// Loop function runs repeatedly after setup
+// During every loop, the program reads temperature, computes PID output, and writes to LCD
 void loop()
 {
+  // Reads input, computes PID, and writes output
   readInput();
   myPID.Compute();
   analogWrite(HEATERPIN, Output);
 
+  // Prints the output percentage to the bottom right of the LCD
   lcd.setCursor(9, 1);
   float outputPercent = Output/255*100;
   lcd.print("O:");
+
+  // Adjusts the number of decimal places depending on the output percentage
   if(outputPercent == 100)
   {
     lcd.print(outputPercent, 0);
@@ -152,6 +177,10 @@ void loop()
     lcd.print(outputPercent, 1);
     lcd.print('%');
   }
+
+  // Prints new line to serial monitor to serperate each loop
   Serial.print("\n");
+
+  // Delays for 100 ms to allow the algorithm and sensors to update
   delay(100);
 }
